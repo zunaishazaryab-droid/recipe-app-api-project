7e68629d3b5cd8054b91448ff1120af814824cfa
Arslan Yousaf <devtechofficial90@gmail.com>
2025-06-16 17:10:12 +0500

    Enhance Dockerfile and docker-compose.yml for PostgreSQL integration; add calc.py and tests for calculator functions

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Function
+
+"""
+
+def add(x,y):
+    """Add X and Y and return Result """
+    
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y and result result. """
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""Sample test"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Calc test module"""
+    
+    def test_add_num(self):
+        """Testing adding number Together"""
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+        
+    def test_subtract_number(self):
+        """Subtracting numbers logic test"""
+        res = calc.subtract(15,10)
+        self.assertEqual(res,5)
+
