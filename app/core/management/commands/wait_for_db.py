d7e68939fc8af87ba870d124d57fbf80bf771f3d
Arslan Yousaf <devtechofficial90@gmail.com>
2025-07-01 15:49:37 +0500

    Refactor wait_for_db command to handle database availability checks; add tests for command functionality and fix docker-compose environment variable syntax

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,26 @@
 """
 Django command to wait for the database to be available
 """
-
+import time
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
 
 class Command(BaseCommand):
     """
     Django command to wait for db
     """
     def handle(self, *args, **options):
-        pass
+        """Entry point for the command. """
+        self.stdout.write("Waiting for the Database. ")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases = ['default'])
+                db_up = True
+            except(Psycopg2OpError, OperationalError):
+                self.stdout.write("Database is unavailable. Waiting 1 Second...")
+                time.sleep(1)
+                
+        self.stdout.write(self.style.SUCCESS('Database Available!'))
